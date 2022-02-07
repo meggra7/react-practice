4a15e131fdf9c83217270b67bdbd2c76f1d1638e
meggra7 <meghan.g.dunham@gmail.com>
2022-02-07 06:48:10 -0500

    Set contacts state in App component
    Refactored contacts list so that it is initialized and accessible
    as a component state rather than a standalone list accessed
    when app first run, only.

--- reactnd-contacts-app/src/App.js
+++ reactnd-contacts-app/src/App.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
- ];
-
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [
+        {
+          "id": "karen",
+          "name": "Karen Isgrigg",
+          "handle": "karen_isgrigg",
+          "avatarURL": "http://localhost:5001/karen.jpg"
+        },
+        {
+          "id": "richard",
+          "name": "Richard Kalehoff",
+          "handle": "richardkalehoff",
+          "avatarURL": "http://localhost:5001/richard.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "handle": "tylermcginnis",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
+      ]
+    }
+  }
+  
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
