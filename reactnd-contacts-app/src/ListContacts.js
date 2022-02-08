690c2d0afb6605cf520f18dc3499507dd88b58e5
meggra7 <meghan.g.dunham@gmail.com>
2022-02-08 06:11:14 -0500

    Implement remove contact
    Added function to App component to allow user to delete a contact
    from the list.  For simplicity's sake, reformatted ListContacts
    to directy map and render each contact instead of referring to
    a separate Contact class.

--- reactnd-contacts-app/src/ListContacts.js
+++ reactnd-contacts-app/src/ListContacts.js
@@ -1,14 +1,28 @@
-import React, {Component} from "react";
-import Contact from "./Contact";
+import React, { Component } from "react";
 
-class ListContacts extends Component {
-    render() {
-        return (
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact) => <Contact key={contact.id} contact={contact} />)}
-            </ol>
-        );
-    }
+function ListContacts(props) {
+    return (
+        <ol className='contact-list'>
+            {props.contacts.map(contact => (
+                <li key={contact.id} className='contact-list-item'>
+                    <div
+                        className='contact-avatar'
+                        style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}
+                    ></div>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button
+                        className="contact-remove"
+                        onClick={() => props.onDeleteContact(contact)}>
+                    </button>
+                </li>
+            ))};
+        </ol>
+    );
 }
 
 export default ListContacts;
